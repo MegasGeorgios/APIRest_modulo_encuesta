40c16cd2e7835cc57b022d16c53123597044b6da
MegasGeorgios <georgaky.71@gmail.com>
2017-12-08 22:14:39 +0100

    gass

--- config/database.php
+++ config/database.php
@@ -26,7 +26,8 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
+    //'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -67,6 +68,16 @@ return [
         ],
 
         'pgsql' => [
+          'driver'   => 'pgsql',
+          'host'     => parse_url(getenv("DATABASE_URL"))["host"],
+          'database' => substr(parse_url(getenv("DATABASE_URL"))["path"], 1),
+          'username' => parse_url(getenv("DATABASE_URL"))["user"],
+          'password' => parse_url(getenv("DATABASE_URL"))["pass"],
+          'charset'  => 'utf8',
+          'prefix'   => '',
+          'schema'   => 'public',
+        ],
+        /*'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
@@ -77,7 +88,7 @@ return [
             'prefix' => '',
             'schema' => 'public',
             'sslmode' => 'prefer',
-        ],
+        ],*/
 
     ],
 
